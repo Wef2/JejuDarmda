973e01432e708c806ae54df782d484e0b8d8022d
Wef2 <qhrud5725@hanmail.net>
2016-12-10 15:17:51 +0900

    Update Daum Login

--- app/src/main/java/com/mcl/jejudarmda/activity/MainActivity.java
+++ app/src/main/java/com/mcl/jejudarmda/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -67,41 +68,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             showToast("로그인 성공");
             LoginStatus.setDaum(true);
             checkAllStatus();
+            MobileOAuthLibrary.getInstance().requestResourceWithPath(getApplicationContext(), oAuthListener, "/user/v1/show.json");
         }
 
         @Override
         public void onAuthorizeFail(OAuthError.OAuthErrorCodes errorCode, String errorMessage) {
             showToast("onAuthorizeFail : " + errorMessage);
-            if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorInvalidAuthorizationRequest)) {
-                // 파라미터를 잘못 사용한 경우.
-            } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorUnauthorizedClient)) {
-                // 승인되지 않은 Client ID 를 사용한 경우         } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorAccessDenied)) {
-                // 사용자가 승인 페이지에서 "취소"를 누른 경우         } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorUnsupportedResponseType)) {
-                // 지원되지 않는 인증방식을 사용한 경우         } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorInvalidScope)) {
-                // 유효한 권한 요청이 아닌 경우.
-            }
         }
 
         @Override
         public void onRequestResourceSuccess(String response) {
-            showToast("onRequestResourceSuccess : " + response);
-            // 결과 피싱은 앱에서 담당한다.
+            Log.i("Response", response);
         }
 
         @Override
         public void onRequestResourceFail(OAuthError.OAuthErrorCodes errorCode, String errorMessage) {
-            showToast("onRequestResourceFail : " + errorMessage);
-            if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorInvalidToken)) {
-                // access token 이 없거나 만료처리된 경우 or 401 에러             // authorize() 를 통해 다시 access token을 발급 받아야함.
-            } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorInvalidResourceRequest)) {
-                // 서버와 통신중 400 에러가 발생한 경우         } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorInsufficientScope)) {
-                // 서버와 통신중 403 에러가 발생한 경우         } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorServiceNotFound)) {
-                // 서버와 통신중 404 에러가 발생한 경우         } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorNetwork)) {
-                // 현재 휴대폰의 네트워크를 이용할 수 없는 경우         } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorServer)) {
-                // 서버쪽에서 에러가 발생하는 경우             // 서버 페이지에 문제가 있는 경우이므로 api 담당자와 얘기해야함.
-            } else if (errorCode.equals(OAuthError.OAuthErrorCodes.OAuthErrorUnknown)) {
-                // 서버와 통신중 그 외 알수 없는 에러가 발생한 경우.
-            }
+            showToast("에러");
+            Log.e("Error", errorMessage);
         }
     };
 
@@ -205,7 +188,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (accessToken != null) {
             LoginStatus.setFacebook(true);
         }
-        
+
+        // Daum Login Check
+        Uri uri = getIntent().getData();
+        if (uri != null) {
+            MobileOAuthLibrary.getInstance().handleUrlScheme(uri);
+        }
+
         checkAllStatus();
 
         // Daum
@@ -235,7 +224,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
         Session.getCurrentSession().removeCallback(callback);
+
+        MobileOAuthLibrary.getInstance().uninitialize();
     }
 
     private void setTextByStatus(boolean status, TextView textView) {
@@ -311,6 +303,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 kakaoLoginDelegate.performClick();
                 break;
             case DAUM:
+                System.out.println("DAUM LOGIN");
                 MobileOAuthLibrary.getInstance().authorize(this, oAuthListener);
                 break;
         }
